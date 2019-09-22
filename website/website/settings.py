76692fd268379541fdb37c3035470871b3624344
李育名 <amrmm1997@gmail.com>
2019-09-22 15:03:16 +0800

    create database

--- website/website/settings.py
+++ website/website/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_tripblog.apps.AppTripblogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,11 +130,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app_tripblog/static'),
     os.path.join(BASE_DIR, 'app_team/static')
 ]
 
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
