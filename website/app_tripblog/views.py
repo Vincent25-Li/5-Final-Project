0f9ad698a7c1b85fdec5fc8364d8539f20e16b89
galaxydrifting <drift2galaxy@gmail.com>
2019-10-16 14:47:11 +0800

    ajax and post for album fixed

--- website/app_tripblog/views.py
+++ website/app_tripblog/views.py
@@ -93,6 +93,7 @@ def login(request):
 def logout(request, user_account=None):
     del request.session['login_user']
     del request.session['is_login']
+    # print(request.META.get('HTTP_REFERER'))
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def article(request, user_account=None, article_id=None):
@@ -197,23 +198,61 @@ def chatbot(request, user_account=None):
         raise Http404
 
 def show_photos(request, user_account=None, albums='albums', album=None):
+    print('get success')
     title = 'Gallery'
     user = User.objects.get(user_account=user_account)
     user_name = user.user_name
     album_path = os.path.join(settings.MEDIA_ROOT, user_account, albums, album)
     relative_path2cat = os.path.join('/media', user_account, albums, album)
     if request.method == 'GET':
-        # return render(request, 'tripblog/gallery.html', locals())
+    #     return render(request, 'tripblog/gallery.html', locals())
     # elif request.method == 'POST':
+        # model_file = os.path.join(settings.MEDIA_ROOT, 
+        #                         'models_weights', 'image_classifier', 'output_graph.pb')
+        # label_file = os.path.join(settings.MEDIA_ROOT, 
+        #                         'models_weights', 'image_classifier', 'output_labels.txt')
+        # img_classifier = Image_Classifier(model_file, label_file, user_account, album)
+
+        # duplicate_imgs = []
+        display_imgs = []
+        # for img in request.FILES.getlist('upload_imgs'):
+        #     img_fp = os.path.join(settings.MEDIA_ROOT, img.name) # img file path
+        #     with open(img_fp, 'wb+') as f:
+        #         for chunk in img.chunks():
+        #             f.write(chunk)
+            
+        #     predict_result = img_classifier.predict(img_fp)
+        #     des = os.path.join(album_path, predict_result)
+        #     duplicate_img = img_classifier.photo2category(img_fp, des)
+        #     if duplicate_img != None: # 判斷重複
+        #         duplicate_imgs.append(duplicate_img)
+        
+        # print(f'duplicate_imgs: {duplicate_imgs}') #暫時不寫
+        for category in os.listdir(album_path):
+            if category == '.DS_Store':
+                continue
+            for image in os.listdir(os.path.join(album_path, category)):
+                if image == '.DS_Store':
+                    continue
+                image_path = os.path.join(relative_path2cat, category, image)
+
+                if settings.MEDIA_ROOT.startswith('C:'): # for windows
+                    image_path = image_path.replace('\\', '/')
+                display_imgs.append(image_path)
+
+        return render(request, 'tripblog/gallery.html', locals())
+    elif request.method == 'POST':
+        print('post success')
         model_file = os.path.join(settings.MEDIA_ROOT, 
                                 'models_weights', 'image_classifier', 'output_graph.pb')
         label_file = os.path.join(settings.MEDIA_ROOT, 
                                 'models_weights', 'image_classifier', 'output_labels.txt')
         img_classifier = Image_Classifier(model_file, label_file, user_account, album)
-
+        print(request.FILES.getlist('upload_imgs'))
         duplicate_imgs = []
         display_imgs = []
         for img in request.FILES.getlist('upload_imgs'):
+            print(img.name)
             img_fp = os.path.join(settings.MEDIA_ROOT, img.name) # img file path
             with open(img_fp, 'wb+') as f:
                 for chunk in img.chunks():
@@ -237,7 +276,6 @@ def show_photos(request, user_account=None, albums='albums', album=None):
                 if settings.MEDIA_ROOT.startswith('C:'): # for windows
                     image_path = image_path.replace('\\', '/')
                 display_imgs.append(image_path)
-
         return render(request, 'tripblog/gallery.html', locals())
 
 def ajax_show_photos(request, user_account=None, albums='albums', album=None, category=None):
