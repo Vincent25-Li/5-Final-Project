34833fb9b14a98aeebfc66c92299e9b5b1c3514d
Jessie <jessie1211su@gmail.com>
2019-10-24 16:10:47 +0800

    update openpose

--- website/app_tripblog/views.py
+++ website/app_tripblog/views.py
@@ -537,10 +537,9 @@ def pose_analysis(request, user_account=None):
         image = np.array(image).reshape(height, width, -1)[:, :, :3]
         image = image.astype('uint8')
         result = openpose_object.openpose_matching(image, user_account)
-        print(result)
 
         response = {}
-        response['response'] = 'OK'
+        response['result'] = result
         return JsonResponse(response)
         
 
